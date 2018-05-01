7b983efe9633ac56c0ca1e3696473cccf5260c78
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-05-01 21:29:46 +0300

    chore(Initialize flask app with basic config)
    [Delivers #157029951]

---
+++ runapp.py
@@ -0,0 +1,10 @@
+import os
+from app import create_app
+
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run('', port=port)
