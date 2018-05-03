6281f2915b4ffd7c6914159dbe47edded30149d7
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-05-03 09:02:56 +0300

    feature(Add jwt decode and encode methods to user model)
    [Delivers #156948596]

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,8 @@
-from app import db, create_app
 from flask_bcrypt import Bcrypt
+from flask import current_app
+from datetime import datetime, timedelta
+import jwt
+from app import db
 
 
 class User(db.Model):
@@ -17,6 +20,45 @@ class User(db.Model):
         self.email = email
         self.password = Bcrypt().generate_password_hash(password).decode()
         self.admin = admin
+
+    def is_password_valid(self, password):
+        """Compare password with the harsh to check validity"""
+        return Bcrypt.check_password_hash(self.password, password)
+
+    def generate_token(self, user_id):
+        """Generate an access token required to log in user"""
+        try:
+            # create a payload to be used in generating token
+
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=60),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+
+            # generate a jwt encoded string
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('SECRET'),
+                algorithm='HS256'
+            )
+            return jwt_string
+        except Exception as e:
+            return str(e)
+
+    @staticmethod
+    def decode_toke(token):
+        """A method to decode access token from header"""
+        try:
+            # decode the token using the SECRET
+            payload = jwt.decode(token, current_app.config.get('SECRET'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            # if the token is expired, return an error string
+            return "Expired token. Please login to get a new token"
+        except jwt.InvalidTokenError:
+            # the token is invalid, return an error string
+            return "Invalid token. Please register or login"
 
     def save(self):
         """Save a user to the database"""
